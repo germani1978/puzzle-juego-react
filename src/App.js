44823caf11ab1b31b88aa506db4cbb86b9a212b9
Germani Benítez Alfonso <germanibenitezalfonso1@gmail.com>
2023-02-02 15:07:22 -0500

    primero

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import './App.css';
+import Nombre from './Nombre';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Nombre/>
+    </div>
+  );
+}
+
+export default App;
+
+
+ 
---
+++ src/Nombre.js
@@ -0,0 +1,10 @@
+
+import { Card, CardContent } from "@mui/material"
+
+export default function Nombre(params) {
+    return <Card sx={{width: '90%', height: '90%'}}>
+        <CardContent>
+        </CardContent>
+    </Card>
+
+}
